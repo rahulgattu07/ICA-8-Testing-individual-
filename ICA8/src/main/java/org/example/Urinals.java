0e964e66e97760ff049601d9a1d748e7d3017664
rahulgattu07 <rgattu1@asu.edu>
2022-10-27 22:49:52 -0700

    GoodString and getUrinals functionalities are implemented

--- ICA8/src/main/java/org/example/Urinals.java
+++ ICA8/src/main/java/org/example/Urinals.java
@@ -1,11 +1,93 @@
 package org.example;
+// Author: Rahul Gattu
+
+import java.util.Scanner;
 
 public class Urinals {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter the input");
+        s=sc.next();
+        goodString("11000");
+        int x=getUrinals(s);
+        System.out.println(x);
     }
-    public static boolean goodString( String str ) {  // checks to see if valid string
-        System.out.println("Not Yet Implemented");
-        return true;
+    public static boolean goodString( String str )
+    {  // checks to see if valid string
+        boolean gs=false;
+       for(int i=0;i<str.length();i++)
+       {
+           if(str.charAt(i)=='0'||str.charAt(i)=='1')
+           {
+               gs=true;
+           }
+           else {
+               gs=false;
+           }
+       }
+        return gs;
+    }
+    public static int getUrinals(String s){
+        int count =0,count1=0;
+        int j=0;
+        if(s.contains("11")){
+            return -1;
+        }
+        for(int l=0;l<s.length();l++){
+            if(s.charAt(l)=='1'){
+                count++;
+            }
+        }
+       // System.out.println(count);
+        char[] c= new char[s.length()];
+        for(int i=0;i<s.length();i++)
+        {
+            c[i]=s.charAt(i);
+        }
+
+        for(int i=0; i<c.length;i++)
+        {
+            if(i==0)
+            {
+                if(c[i]=='0' && c[i+1]=='0'){
+                    c[i]='1';
+                }
+            }
+            else if(i==c.length-1){
+                if(c[i]=='0' && c[i-1]=='0'){
+                    c[i]='1';
+                }
+
+
+            }
+            else{
+                if(c[i]=='0' && c[i-1]=='0' && c[i+1]=='0'){
+                    c[i]='1';
+
+                }
+                else if(c[i]=='1' && c[i+1]=='1'){
+                    return -1;
+
+
+                }
+            }
+        }
+        for(int i=0;i<c.length;i++){
+            if(c[i]=='1'){
+                count1++;
+
+            }
+
+        }
+        for(int i=0;i<c.length;i++){
+
+
+            //System.out.println(c[i]);
+
+        }
+        //System.out.println(count1);
+        //System.out.println(count);
+        return count1-count;
     }
 }
--- ICA8/src/test/java/org/example/UrinalsTest.java
+++ ICA8/src/test/java/org/example/UrinalsTest.java
@@ -14,6 +14,17 @@ class UrinalsTest {
     void goodString() {
 
         System.out.println("====== Rahul Gattu == TEST ONE EXECUTED =======");
-        assertEquals(false, Urinals.goodString("110"));
+        assertEquals(false, Urinals.goodString("abc"));
+    }
+
+    @Test
+    void getUrinals()
+    {
+        assertEquals(1,Urinals.getUrinals("10001"));
+        assertEquals(0,Urinals.getUrinals("1001"));
+        assertEquals(3,Urinals.getUrinals("00000"));
+        assertEquals(2,Urinals.getUrinals("0000"));
+        assertEquals(1,Urinals.getUrinals("01000"));
+        assertEquals(-1,Urinals.getUrinals("011"));
     }
 }
